abfd92370187e857742c9c3903e354ee67c15c98
DeTanvir18 <tanvirahmed18.ta@gmail.com>
2023-12-12 11:14:30 +0600

    some corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import About from "./components/About";
 import Banner from "./components/Banner";
 import Contact from "./components/Contact";
@@ -5,19 +6,45 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Services from "./components/Services";
+import { BallTriangle } from "react-loader-spinner";
 
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500)
+  }, [])
   return (
-    <div className="bg-slate-800">
-      <Navbar></Navbar>
-      <Banner></Banner>
-      <About></About>
-      <Services></Services>
-      <Projects></Projects>
-      <Contact></Contact>
-      <Footer></Footer>
-    </div>
+    <>
+      {
+        loading ?
+          <div className="bg-slate-900 h-screen flex justify-center items-center">
+            <BallTriangle
+              height={100}
+              width={100}
+              radius={5}
+              color="#fa19ef"
+              ariaLabel="ball-triangle-loading"
+              wrapperClass={{}}
+              wrapperStyle=""
+              visible={true}
+            />
+          </div>
+          :
+          <div className="bg-slate-800">
+            <Navbar></Navbar>
+            <Banner></Banner>
+            <About></About>
+            <Services></Services>
+            <Projects></Projects>
+            <Contact></Contact>
+            <Footer></Footer>
+          </div>
+      }
+    </>
   );
 };
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,10 +4,10 @@ const Services = () => {
         <div id="Services" className="p-20 flex flex-col justify-center items-center">
             <h1 className="text-[52px] font-semibold mb-20 leading-normal uppercase text-fuchsia-500">Services</h1>
             <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 justify-around gap-20">
-                <h2 data-aos="fade-up" className="text-[26px] flex items-center justify-center font-semibold text-fuchsia-500 rounded-3xl h-36 w-44 border-2 border-fuchsia-500 b_glow">HTML5</h2>
-                <h2 data-aos="fade-down" className="text-[26px] flex items-center justify-center font-semibold text-fuchsia-500 rounded-3xl h-36 w-44 border-2 border-fuchsia-500 b_glow">CSS3</h2>
-                <h2 data-aos="fade-up" className="text-[26px] flex items-center justify-center font-semibold text-fuchsia-500 rounded-3xl h-36 w-44 border-2 border-fuchsia-500 b_glow">JavaScript</h2>
-                <h2 data-aos="fade-down" className="text-[26px] flex items-center justify-center font-semibold text-fuchsia-500 rounded-3xl h-36 w-44 border-2 border-fuchsia-500 b_glow">React</h2>
+                <h2 data-aos="fade-up" className="text-[26px] flex items-center justify-center font-semibold text-fuchsia-500 rounded-ss-3xl rounded-br-3xl h-36 w-44 border-2 border-fuchsia-500 b_glow">HTML5</h2>
+                <h2 data-aos="fade-down" className="text-[26px] flex items-center justify-center font-semibold text-fuchsia-500 rounded-ss-3xl rounded-br-3xl h-36 w-44 border-2 border-fuchsia-500 b_glow">CSS3</h2>
+                <h2 data-aos="fade-up" className="text-[26px] flex items-center justify-center font-semibold text-fuchsia-500 rounded-ss-3xl rounded-br-3xl h-36 w-44 border-2 border-fuchsia-500 b_glow">JavaScript</h2>
+                <h2 data-aos="fade-down" className="text-[26px] flex items-center justify-center font-semibold text-fuchsia-500 rounded-ss-3xl rounded-br-3xl h-36 w-44 border-2 border-fuchsia-500 b_glow">React</h2>
             </div>
         </div>
     );
